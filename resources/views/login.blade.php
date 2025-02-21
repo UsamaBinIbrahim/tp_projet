20858c9188bf87fed08ca2ac52807bd36731a83d
Usamah <usamah@gmail.com>
2025-02-21 21:08:09 +0200

    Initial commit
    this commit includes the following:
    - Created the project
    - Added a route for login
    - Created a UserController with login action
    - Created login view

---
+++ resources/views/login.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div>
+        <h2>Login</h2>
+        <form action="" method="POST">
+            @csrf
+            <div>
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" required>
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" required>
+            </div>
+            <button type="submit">Login</button>
+        </form>
+        <p><a href="">Don't have an account? Create new one </a></p>
