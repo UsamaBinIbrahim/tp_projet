eeea2e19ea714b2774caddc244cbbc47451fc062
Usamah <usamah@gmail.com>
2025-02-24 21:43:06 +0200

    Dashboard Interface
    this commit includes the following:
    - added bootstrape package to public directory
    - added some images to public directory for testing
    - created static posts array to test dashboard interface
    - created a temporary inteface for dashboard

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,21 @@ use App\Models\User;
 class UserController extends Controller
 {
     public function dashboard(User $user) {
-        return view('user.dashboard', ['user_id' => $user->id]);
+        $posts = [
+            [
+                'user_id' => 1,
+                'description' => 'This is my first post.',
+                'image' => '1.jpg',
+                'created_at' => '02-01-2020', 
+            ],
+            [
+                'user_id' => 3,
+                'description' => 'This is my first post.',
+                'image' => '1.jpg',
+                'created_at' => '02-01-2020', 
+            ],
+        ];
+
+        return view('user.dashboard', ['posts' => $posts]);
     }
 }
--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,1 +1,52 @@
-This is bashboard view of user {{$user_id}}
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <style>
+        .navbar ul {
+            display: flex;
+            justify-content: flex-start;
+            list-style: none;
+            padding: 0 20px;
+            width: 100%;
+        }
+        .navbar ul li {
+            margin-right: 20px;
+        }
+        .navbar ul li a {
+            text-decoration: none;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <ul>
+            <li><a href="">Dashboard</a></li>
+            <li><a href="">Profile</a></li>
+            <li><a href="">Settings</a></li>
+            <li><a href="">Logout</a></li>
+        </ul>
+    </nav>
+    <hr>
+    @foreach ($posts as $post)
+        <div>
+            <div style="display: flex; justify-content: space-around">
+                <div> {{$post['user_id']}} </div>
+                <div> {{$post['created_at']}} </div>
+            </div>
+            <div style="display: flex; justify-content: center">
+                <div> {{$post['description']}} </div>
+            </div>
+            <div style=" display:flex;justify-content:center">
+                <img src="{{asset('/img/desktop_shelfs.jpeg')}}" alt="" style="width: 150px; height: 150px;">
+            </div>
+        </div>
+    @endforeach
+    
+    <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+</body>
+</html>
