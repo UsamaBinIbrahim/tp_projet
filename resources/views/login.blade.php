c8c0912b4421cf446b01591384000ee186b4c137
Usamah <usamah@gmail.com>
2025-02-25 13:19:05 +0200

    Login and Register styling
    this commit includes the following:
    - create style folders bootstrap, login and user inside the public/css/
      directory
    - move all css bootstrap files to public/css/bootstrap
    - style login and register views, and move the css code to public/css/login
      folder
    - move the css code for user dashboard to public/css/user folder

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="{{asset('css/login/style.css')}}">
 </head>
 <body>
-    <div>
+    <div class="login-container">
         <h2>Login</h2>
         <form action="{{route('login')}}" method="POST">
             @csrf
@@ -21,10 +22,11 @@
             <button type="submit">Login</button>
         </form>
         <p><a href="{{route('register')}}">Don't have an account? Create one now</a></p>
+        @if(session('login'))
+            <div class="error-message">
+                {{session('login')}}
+            </div>
+        @endif
     </div>
-    @session('login')
-    <div style="background-color: lightcoral; margin: 10px; padding:5px; color:rgb(88, 5, 5)">
-        {{session('login')}}
-    </div>
-    @endsession
 </body>
+</html>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,48 +4,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
+    <link rel="stylesheet" href="{{asset('css/login/style.css')}}">
+    <style>
+    </style>
 </head>
 <body>
-    <div>
+    <div class="login-container">
         <h2>Register</h2>
         <form action="{{route('register.store')}}" method="POST">
             @csrf
-            <div>
-                <label for="firstName">First Name</label>
-                <input type="text" value="{{$errors->any()? old('firstName') : null}}" name="firstName" id="firstName" required>
+            <div class="form-row">
+                <div>
+                    <label for="firstName">First Name</label>
+                    <input type="text" value="{{$errors->any()? old('firstName') : null}}" name="firstName" id="firstName" required>
+                </div>
+                <div>
+                    <label for="lastName">Last Name</label>
+                    <input type="text" value="{{$errors->any()? old('lastName') : null}}" name="lastName" id="lastName" required>
+                </div>
             </div>
-            <div>
-                <label for="lastName">Last Name</label>
-                <input type="text" value="{{$errors->any()? old('lastName') : null}}" name="lastName" id="lastName" required>
+            <div class="form-row">
+                <div>
+                    <label for="username">Username</label>
+                    <input type="text" value="{{$errors->any()? old('username') : null}}" name="username" id="username" required>
+                </div>
+                <div>
+                    <label for="email">Email</label>
+                    <input type="text" value="{{$errors->any()? old('email') : null}}" name="email" id="email" required>
+                </div>
             </div>
-            <div>
-                <label for="username">Username</label>
-                <input type="text" value="{{$errors->any()? old('username') : null}}" name="username" id="username" required>
-            </div>
-            <div>
-                <label for="email">Email</label>
-                <input type="text" value="{{$errors->any()? old('email') : null}}" name="email" id="email" required>
-            </div>
-            <div>
-                <label for="password">Password</label>
-                <input type="password" value="" name="password" id="password" required>
-            </div>
-            <div>
-                <label for="password">Confirm Password</label>
-                <input type="password" name="password_confirmation" id="password_confirmation" required>
+            <div class="form-row">
+                <div>
+                    <label for="password">Password</label>
+                    <input type="password" value="" name="password" id="password" required>
+                </div>
+                <div>
+                    <label for="password_confirmation">Confirm Password</label>
+                    <input type="password" name="password_confirmation" id="password_confirmation" required>
+                </div>
             </div>
             <button type="submit">Register</button>
         </form>
         <p><a href="{{route('index')}}">Login</a></p>
-    </div>
-    @if ($errors->any())
-    <div style="background-color: lightcoral; margin: 10px; padding:5px; color:rgb(88, 5, 5)">
-        <p>Errors: </p>
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li> {{$error}} </li>
-            @endforeach
-        </ul>
+        @if ($errors->any())
+        <div class="error-message">
+            <p>Invalid Inputs: </p>
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li> {{$error}} </li>
+                @endforeach
+            </ul>
+        </div>
     </div>
     @endif
 </body>
